a76e679ab5b253ee704a21fb4c32d039899788ec
evMSS16 <eva.solis@udem.com>
2024-01-16 14:32:07 -0600

    Parte 12

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
   {path: '', component: HomeComponent},
   {path: 'test-error', component: TestErrorComponent},
   {path: 'not-found', component: NotFoundComponent},
